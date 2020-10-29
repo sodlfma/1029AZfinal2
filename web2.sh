9afc073f55ff3644c22f52707b3ff1af93876c55
sodlfma <44385154+sodlfma@users.noreply.github.com>
2020-10-29 14:12:07 +0900

    Update and rename web2_nginx.sh to web2.sh

---
+++ web2.sh
@@ -0,0 +1,8 @@
+sudo apt-get update -y
+sudo apt-get install -y apache2
+
+sudo echo "<html>" > /var/www/html/index.html
+sudo echo "<h1>Server2</h1>" >> /var/www/html/index.html
+sudo echo "</html>" >> /var/www/html/index.html
+
+sudo service apache2 restart
