6f9f54e5e5c5c91edd7ed8464f7cbfa464af683f
sodlfma <44385154+sodlfma@users.noreply.github.com>
2020-10-29 10:28:35 +0900

    s

---
+++ web2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo su -
+apt-get update -y
+apt-get install -y apache2
+
+echo "<html>" > /var/www/html/index.html
+echo "<h1>Server2</h1>" >> /var/www/html/index.html
+echo "</html>" >> /var/www/html/index.html
+
+service apache2 restart
