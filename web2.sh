3684b85b18a6844a420df97f82dd01c8c8fd06a6
sodlfma <44385154+sodlfma@users.noreply.github.com>
2020-10-29 11:36:56 +0900

    Update web2.sh

--- web2.sh
+++ web2.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+apt-get update -y
+apt-get install -y apache2
 
-sudo apt-get update -y
-sudo apt-get install -y apache2
+echo "<html>" > /var/www/html/index.html
+echo "<h1>Server2</h1>" >> /var/www/html/index.html
+echo "</html>" >> /var/www/html/index.html
 
-sudo echo "<html>" > /var/www/html/index.html
-sudo echo "<h1>Server2</h1>" >> /var/www/html/index.html
-sudo echo "</html>" >> /var/www/html/index.html
-
-sudo service apache2 restart
+service apache2 restart
