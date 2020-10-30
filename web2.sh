8ce328d0607f6126ab71690e882ff030c03cd3cf
sodlfma <44385154+sodlfma@users.noreply.github.com>
2020-10-30 12:54:52 +0900

    Update web2.sh

--- web2.sh
+++ web2.sh
@@ -3,6 +3,9 @@ sudo apt-get install -y apache2
 
 sudo echo "<html>" > /var/www/html/index.html
 sudo echo "<h1>Server2</h1>" >> /var/www/html/index.html
+echo "<img src=\"https://pbs.twimg.com/profile_images/745938977413201920/-OV6pONi_400x400.jpg\">" >> /var/www/html/index.html
 sudo echo "</html>" >> /var/www/html/index.html
 
 sudo service apache2 restart
+
+
